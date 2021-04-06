55973c4cec1f363903a84771fd2856f448192463
SophieeV <sophievesin@wanadoo.fr>
2021-04-06 10:43:23 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function Border() {
+  document.getElementById('img1').style.border = 'thick solid #ff0000';
+}
+function NoBorder() {
+  document.getElementById('img1').style.border = 'thick solid #ffffff';
+}
